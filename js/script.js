d6ff6db06d4cda98a4b4d96c5c76d9ed284ede4a
Evgenya Pankratova <janice0free@gmail.com>
2023-03-25 20:46:29 +0500

    Добавление стилей для карточек

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 ////////Строительство//////////////
 let title = document.querySelector('.aside__list-title');
 let list = document.querySelectorAll('.aside__list-item');
+let icon = document.querySelector('.list-title-icon');
 
 function showList(){
-    for(let i=0; i < list.length; i++){
+    for(let i=0; i < list.length; i++){ //скрываем/показываем элементы списка
         list[i].hidden = !list[i].hidden;
     }
+    icon.classList.toggle('list-title-icon-rotate'); //поворачиваем стрелку при закрытии списка
 };
 
 title.addEventListener("click", showList);
@@ -13,11 +15,13 @@ title.addEventListener("click", showList);
 ////////Проекты//////////////
 let titleTwo = document.querySelector('.aside__list-titleTwo');
 let listTwo = document.querySelectorAll('.aside__list-itemTwo');
+let iconTwo = document.querySelector('.list-title-iconTwo');
 
 function showListTwo(){
     for(let i=0; i < listTwo.length; i++){
         listTwo[i].hidden = !listTwo[i].hidden;
     }
+    iconTwo.classList.toggle('list-title-icon-rotate');
 };
 
 titleTwo.addEventListener("click", showListTwo);
@@ -25,11 +29,13 @@ titleTwo.addEventListener("click", showListTwo);
 ////////Дома от 200м2//////////
 let titleThree = document.querySelector('.aside__list-titleThree');
 let listThree = document.querySelectorAll('.aside__list-itemThree');
+let iconThree = document.querySelector('.list-title-iconThree');
 
 function showListThree(){
     for(let i=0; i < listThree.length; i++){
         listThree[i].hidden = !listThree[i].hidden;
     }
+    iconThree.classList.toggle('list-title-icon-rotate');
 };
 
 titleThree.addEventListener("click", showListThree);
